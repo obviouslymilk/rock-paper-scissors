619a58180a31ca5fd5f70e1bf75c9e7351345a0b
Klim Storozhenko <68001894+obviouslymilk@users.noreply.github.com>
2024-01-23 12:16:57 +0300

    Create round logic

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function getPlayerChoice() {
     }
     return choosedObject;
 }
+
+function checkWinCondition(playerSelection, computerSelection, selectedObject) {
+    if (computerSelection === selectedObject) {
+        return `You win! Your ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You loose! Computer beats your ${playerSelection} with ${computerSelection}`
+    }   
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `Tie! Everyone has selected ${playerSelection}`
+    }
+
+    switch (playerSelection) {
+        case objects[0]:
+            return checkWinCondition(playerSelection, computerSelection, objects[2])
+        case objects[1]:
+            return checkWinCondition(playerSelection, computerSelection, objects[0])
+        case objects[2]:
+            return checkWinCondition(playerSelection, computerSelection, objects[1])
+        default:
+            break;
+    }
+}
