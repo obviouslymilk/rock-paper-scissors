c74148a7063bc341425032ea9ddbab4b0660d4d3
Klim Storozhenko <68001894+obviouslymilk@users.noreply.github.com>
2024-01-26 10:05:41 +0300

    Connect ui with the game logic.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const buttonsElement = document.querySelector('#buttons');
+const scoreLabel = document.querySelector('#status');
+const playerImage = document.querySelector('#player-image');
+const computerImage = document.querySelector('#computer-image');
+
 const objects = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
@@ -6,43 +11,39 @@ function getComputerChoice() {
     return objects[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice() {
-    const choosedObject = prompt("Select rock, paper or scissors.").toLowerCase();
-    if (objects.find((v, i) => v === choosedObject ? true : false) === undefined) {
-        return getPlayerChoice()
-    }
-    return choosedObject;
-}
 
 function checkWinCondition(playerSelection, computerSelection, selectedObject) {
     if (computerSelection === selectedObject) {
         playerScore += 1;
-        return `You win! Your ${playerSelection} beats ${computerSelection}.`
+        return `You win! Your ${playerSelection} beats ${computerSelection}. Score: ${playerScore} : ${computerScore}`;
     } else {
         computerScore += 1;
-        return `You loose! Computer beats your ${playerSelection} with ${computerSelection}.`
+        return `You loose! Computer beats your ${playerSelection} with ${computerSelection}. Score: ${playerScore} : ${computerScore}`;
     }   
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `Tie! Everyone has selected ${playerSelection}.`
+        return `Tie! Everyone has selected ${playerSelection}. Score: ${playerScore} : ${computerScore}`;
     }
 
     switch (playerSelection) {
         case objects[0]:
-            return checkWinCondition(playerSelection, computerSelection, objects[2])
+            return checkWinCondition(playerSelection, computerSelection, objects[2]);
         case objects[1]:
-            return checkWinCondition(playerSelection, computerSelection, objects[0])
+            return checkWinCondition(playerSelection, computerSelection, objects[0]);
         case objects[2]:
-            return checkWinCondition(playerSelection, computerSelection, objects[1])
+            return checkWinCondition(playerSelection, computerSelection, objects[1]);
         default:
             break;
     }
 }
 
-function game() {
-    while (playerScore + computerScore < 5) {
-        alert(playRound(getPlayerChoice(), getComputerChoice()))
-    }   
-}
+buttonsElement.addEventListener('click', event => {
+    if (event.target.className != "button-image") return ;
+    const playerChoise = event.target.closest('.object-button').id;
+    const computerChoise = getComputerChoice();
+    scoreLabel.textContent = playRound(playerChoise, computerChoise)
+    playerImage.src = `./images/${playerChoise}.svg`;
+    computerImage.src = `./images/${computerChoise}.svg`;
+})
